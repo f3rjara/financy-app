911986787bfafa4b9ab0decdf4dd1a8501cbda56
Fernando Jaramillo <fernando.jaramillo@pragma.com.co>
2022-09-15 15:46:11 -0500

    customize project for

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app/app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { IngresoEgresoComponent } from './ingreso-egreso/ingreso-egreso.component';
-import { EstadisticaComponent } from './ingreso-egreso/estadistica/estadistica.component';
-import { DetalleComponent } from './ingreso-egreso/detalle/detalle.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { IngresoEgresoComponent } from './modules/ingreso-egreso/ingreso-egreso.component';
+import { EstadisticaComponent } from './modules/ingreso-egreso/estadistica/estadistica.component';
+import { DetalleComponent } from './modules/ingreso-egreso/detalle/detalle.component';
 
 
 @NgModule({
